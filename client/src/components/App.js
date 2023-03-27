5f7952fac37d144629a61202c0d57031d54c680c
Andrewstown <stingeyandrewstown@live.com>
2023-03-27 13:44:26 -0500

    Front End

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
 
-function App() {
+export default function App() {
   // Code goes here!
 }
-
-export default App;
