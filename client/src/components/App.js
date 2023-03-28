6e5068607ae34bacd0d460d71ecebddee63799e3
duanegrell <duanegrell@gmail.com>
2023-03-28 11:33:01 -0400

    started adding components

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,30 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import { Link, Route, Switch } from "react-router-dom";
 
-export default function App() {
-  // Code goes here!
+import Home from "./Home"
+import User from "./User";
+import Game from "./Game";
+
+function App() {
+  return (
+    <main>
+      <h1>
+        <Link to="/">We are better than Steam</Link>
+      </h1>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route exact path="/user/:id">
+          <User />
+        </Route>
+        <Route exact path="/game/:id">
+          <Game />
+        </Route>
+      </Switch>
+    </main>
+  );
 }
+
+export default App;
