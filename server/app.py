30de2b9ce536261498a0ae87235a401485cfdc30
Jim Brennan <jim.j.brennan@gmail.com>
2023-03-28 02:25:58 -0400

    made changed to the models and app.py

--- server/app.py
+++ server/app.py
@@ -3,13 +3,240 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
-from flask_restful import Resource
+from flask import Flask, make_response, request, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
 # Local imports
 from config import app, db, api
+from models import db, User, Game, UserGames
+
+#this was in the doc so I'm adding it
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
+api = Api(app)
+
 
 # Views go here!
+
+#GET /users
+class Users(Resource):
+    def get(self):
+
+        user_list = [user.to_dict() for user in User.query.all()]
+
+        response = make_response(user_list, 200)
+
+        return response
+
+#POST /users
+    def post(self):
+        try:
+            new_user = User(
+                name = request.get_json()('name'),
+                email = request.get_json()('email'),
+                password = request.get_json()('password')
+                picture = request.get_json()('picture')
+            )
+
+            new_user_dict = new_user.to_dict()
+
+            db.session.add(new_user)
+            db.session.commit()
+
+            response = make_response(jsonify(new_user_dict), 201)
+
+            return response
+        
+        except ValueError:
+            response = make_response({"errors": ["validation errors"]}, 400)
+
+            return response        
+
+api.add_resource(Users, '/users')
+
+
+#GET /users/:id
+
+class UserById(Resource):
+    def get(self, id):
+
+        user = User.query.filter(User.id == id).first().to_dict()
+
+        if not user:
+            return make_response({
+                "error": "User not found"
+            }, 404)
+        else:
+            response = make_response(user, 200)
+
+            return response
+
+#DELETE /users/:id
+
+    def delete_user(self, id):
+        user = User.query.filter_by(id = id).first()
+
+        if user:
+
+            db.session.delete(user)
+            db.session.commit()
+
+            response = make_response(
+                "",
+                204
+
+            )
+
+        else:
+
+            response = make_response(
+                {"error": "user not found"},
+                404
+            )
+
+        return response
+
+#PATCH /users/:id
+    def patch(self, id):
+
+        user = User.query.filter(User.id == id).first()
+
+        if not user:
+            return make_response({ "error": "User not found" }, 404)
+
+        try: 
+            request_json = request.get_json()
+            for key in request_json:
+                setattr(user, key, request_json[key])
+        
+        
+            db.session.add(user)
+            db.session.commit()
+
+            response = make_response(user.to_dict(), 200)
+
+        except ValueError:
+            response = make_response({
+                "error": "Invalid input"
+            }, 400)
+
+        return response    
+
+api.add_resource(UserById, '/users/<int:id>')
+
+
+#GET /games
+class Games(Resource):
+    def get(self):
+
+        game_list = [game.to_dict() for game in Game.query.all()]
+
+        response = make_response(game_list, 200)
+
+        return response
+
+
+#POST /games
+    def post(self):
+        try:
+            new_game = Game(
+                title = request.get_json()('title'),
+                price = request.get_json()('price'),
+                genre = request.get_json()('genre')
+                description = request.get_json()('description')
+            )
+
+            new_game_dict = new_game.to_dict()
+
+            db.session.add(new_game)
+            db.session.commit()
+
+            response = make_response(jsonify(new_game_dict), 201)
+
+            return response
+        
+        except ValueError:
+            response = make_response({"errors": ["validation errors"]}, 400)
+
+            return response   
+
+api.add_resource(Games, '/games')
+
+
+#GET /games/:id
+
+class GamesById(Resource):
+    def get(self, id):
+
+        game = Game.query.filter(Game.id == id).first().to_dict()
+
+        response = make_response(game, 200)
+
+        return response
+
+#DELETE /games/:id
+
+    def delete_game(self, id):
+        game = Game.query.filter_by(id = id).first()
+
+        if game:
+
+            db.session.delete(game)
+            db.session.commit()
+
+            response = make_response(
+                "",
+                204
+
+            )
+
+        else:
+
+            response = make_response(
+                {"error": "Game not found"},
+                404
+            )
+
+        return response
+
+# PATCH /games/:id
+
+    def patch(self, id):
+
+        game = Game.query.filter(Game.id == id).first()
+
+        if not game:
+            return make_response({ "error": "Game not found" }, 404)
+
+        try: 
+            request_json = request.get_json()
+            for key in request_json:
+                setattr(game, key, request_json[key])
+        
+        
+            db.session.add(game)
+            db.session.commit()
+
+            response = make_response(game.to_dict(), 200)
+
+        except ValueError:
+            response = make_response({
+                "error": "Invalid input"
+            }, 400)
+
+        return response  
+
+api.add_resource(GameById, '/games/<int:id>')
+
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,5 +1,90 @@
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+from sqlalchemy.orm import validates
+from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy_serializer import SerializerMixin
 
 from config import db
 
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+
+db = SQLAlchemy(metadata=metadata)
+
 # Models go here!
+class User(db.Model, SerializerMixin):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String)
+    password = db.Column(db.String)
+    picture = db.Column(db.String)
+    bio = db.Column(db.String)    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    user_games = db.relationship('UserGame', backref='user')
+
+    serialize_rules = ('-user_games.user', '-games.users','-created_at', '-updated_at')
+
+    games = association_proxy('user_games', 'game')
+
+    def __repr__(self):
+        return f'<user name:{self.name}, email:{self.address}, >'
+
+
+class Game(db.Model, SerializerMixin):
+    __tablename__ = "games"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String)
+    #price should prob be float with 2 decimals but I'm tired
+    price = db.Column(db.Integer)
+    genre = db.Column(db.String)
+    description = db.Column(db.String)    
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f'<game title:{self.title}, price:{self.price}, genre: {self.genre}, description: {self.description}>'
+
+    user_games = db.relationship('UserGame', backref='game')
+
+    serialize_rules = ('-user_games.game', '-users.games','-created_at', '-updated_at')
+
+    users = association_proxy('user_games', 'user')
+
+    @validates('price')
+    def validate_price(self, key, value):
+        if value < 0:
+            raise ValueError("must have a valid price")
+        return value
+
+    @validates('description')  
+    def validate_description(self, key, description):
+        if not description:
+            raise ValueError('Description must be present.')
+        
+        if len(description) < 20:
+            raise ValueError('the description must be at least 20 characters')
+        return description
+
+
+
+
+class UserGame(db.Model, SerializerMixin):
+    __tablename__ = "user_games"
+
+    id = db.Column(db.Integer, primary_key=True)
+    hours_played = db.Column(db.Integer)
+    last_played = db.Column(db.DateTime)
+    user_rating = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+    
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    game_id = db.Column(db.Integer, db.ForeignKey('games.id'))
+
+    serialize_rules = ('-user.games', '-games.user', '-user.user_games', 'game.user_games','-created_at', '-updated_at')
