62be6daf254f9ad28deb5ba80b9e89f5518cd951
duanegrell <duanegrell@gmail.com>
2023-03-29 14:38:24 -0400

    Updated users

--- client/src/components/Users.js
+++ client/src/components/Users.js
@@ -1,10 +1,21 @@
 import React, {useState} from "react";
 import UserContainer from "./UserContainer";
 
+// function Users( {users} ) {
+  
+//   const usercards = users.map((user) => (
+//     <UserContainer user = {user}/>
+//   ))
+
+//   return (
+//     <ul className="cards">{usercards}</ul>
+//   );
+// }
+
 function Users( {users} ) {
   
-  const usercards = users.map((user) => (
-    <UserContainer user = {user}/>
+  const usercards = ((users) => (
+    <UserContainer users = {users}/>
   ))
 
   return (
