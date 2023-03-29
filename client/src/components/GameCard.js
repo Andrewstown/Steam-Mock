207d236ef2361c3818dfe7c439e078cac4a8bdbd
duanegrell <duanegrell@gmail.com>
2023-03-29 11:55:04 -0400

    updated game library

--- client/src/components/GameCard.js
+++ client/src/components/GameCard.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import GameLibrary from "./GameLibrary";
 
-function GameCard() {
+function GameCard( ) {
   const [{ data: game, error, status }, setGame] = useState({
     data: null,
     error: null,
