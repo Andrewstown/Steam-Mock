9aa3d6c7db3a200e2f2dfbd22fb6c521e8560af3
jbrenna4 <116452672+jbrenna4@users.noreply.github.com>
2023-03-31 13:20:13 -0400

    updated profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,5 +2,5 @@ import React from "react"
 import ProfileCard from "./ProfileCard"
 
 export default function Profile({user, updateUsers}){
-  return (<ul className="cards"><ProfileCard user={user} updateUsers={updateUsers}/></ul>)
+  return (<ul className="card"><ProfileCard user={user} updateUsers={updateUsers}/></ul>)
 }
