67771d5cc7b885a1e1d458e59636371b0083d275
duanegrell <duanegrell@gmail.com>
2023-03-30 21:13:38 -0400

    updated profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,6 +1,6 @@
 import React from "react"
 import ProfileCard from "./ProfileCard"
-import './Community.css'
+
 
 export default function Profile({user}){
   return (<ul className="cards"><ProfileCard user={user}/></ul>)
